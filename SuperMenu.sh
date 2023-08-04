64bacb2961a4ebf552d6a0cfd4e4871209799110
HuShaker <hushaker.dev@gmail.com>
2023-08-04 05:15:47 +0300

    create function super_menu

--- SuperMenu.sh
+++ SuperMenu.sh
@@ -11,17 +11,30 @@ echo    "║                              Hello Suders !!                       
 echo    "║  Note: Now you have become Suder, but of course, you are not like Eng.Mina  ║"
 echo -e "╚═════════════════════════════════════════════════════════════════════════════╝\n"
 
-echo "+==================================+"
-echo "|          Super Menu              |"
-echo "+==================================+"
-echo "| Option |     Description         |"
-echo "+--------+-------------------------+"
-echo "|   1    | Create Database         |"
-echo "|   2    | List Databases          |"
-echo "|   3    | Drop Database           |"
-echo "|   4    | Connect To Databases    |"
-echo "+--------+-------------------------+"
 
+function super_menu {
+    echo "+==================================+"
+    echo "|          Super Menu              |"
+    echo "+==================================+"
+    echo "| Option |     Description         |"
+    echo "+--------+-------------------------+"
+    echo "|   1    | Create Database         |"
+    echo "|   2    | List Databases          |"
+    echo "|   3    | Drop Database           |"
+    echo "|   4    | Connect To Databases    |"
+    echo "|   5    | exit                    |"
+    echo "+--------+-------------------------+"
 
-
+    echo -e "Enter Choice: \c"
+    read smenu
+    case $smenu in
+        1) echo "create_database" ;;
+        2) list_databases ;;
+        3) drop_database ;;
+        4) connect_to_databases ;;
+        5) exit ;;
+        *) echo " Wrong Choice " ; super_menu;
+    esac
+}
+super_menu
 
