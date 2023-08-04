f950c5c9b41d629d0fcd3dee15132535cba5ea64
shereenm1521 <shereinmakram@gmail.com>
2023-08-04 21:35:38 +0300

    connect modified

--- Database_Functions/Connect_db.sh
+++ Database_Functions/Connect_db.sh
@@ -10,7 +10,7 @@ echo " Select your database number from the minu :"
 
 echo 
 cd ./Databases
-array=('ls -F | grep "/"')
+array=$('ls -F | grep "/"')
 
 
 select choise in ${array[*]}
@@ -40,6 +40,41 @@ cd -
 #---------------------------------------------------------
 
 
+function fun_super_menu {
+    
+    
+    echo "+==================================+"
+    echo "|          Super Menu              |"
+    echo "+==================================+"
+    echo "| Option |     Description         |"
+    echo "+--------+-------------------------+"
+    echo "|   1    | Create table            |"
+    echo "|   2    | List tables             |"
+    echo "|   3    | Drop table              |"
+    echo "|   4    | Select from table       |"
+    echo "|   5    | Delete from table       |"
+    echo "|   6    | Update table            |"
+    echo "|   7    |  Exit                   |"
+    echo "+--------+-------------------------+"
 
+    echo -e "Enter Choice: \c"
+    read smenu
+    case $smenu in
+        1) fun_Create_table      ;;
+        2) fun_list__tables      ;;
+        3) fun_drop_table        ;;
+        4) fun_select_from_table ;;
+        5) fun_delete_table      ;;
+        6) fun_update_table      ;;
+        7_ Exit                  ;;
+        *)
+
+        clear
+        header_note
+        fun_error "Error: *** Wrong Choice *** "
+        fun_super_menu
+    esac
+}
+fun_super_menu
 
 
