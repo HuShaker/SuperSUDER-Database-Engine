4d60ea7543295362944b002b15659ebc46dec5df
shereenm1521 <shereinmakram@gmail.com>
2023-08-04 19:52:20 +0300

    push connect scrito

---
+++ Connect_db.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+
+PS3 = "Type Your DB NUmber to connect with :"
+
+echo 
+
+echo " Select your database number from the minu :"
+
+echo 
+cd . ./databases
+array=('ls -F | grep "/"')
+
+
+select choise in ${array[*]}
+
+do 
+   if [$REPLY -gt ${#array[*]}]
+   then 
+
+      echo "$REPLY not on the minu "
+      continue 
+
+      else 
+         cd ../databases/${array[${REPLY}-1]}
+
+         echo " you are connected to the database "
+
+         echo
+         break
+       fi 
+
+   done 
+       echo
+
+cd -
+
+
+#---------------------------------------------------------
+
+
+
