feb8a0ec887c7da95bfd400d13890136c74dafe4
HuShaker <hushaker.dev@gmail.com>
2023-08-04 04:57:44 +0300

    Add Super menu

--- SuperMenu.sh
+++ SuperMenu.sh
@@ -11,6 +11,16 @@ echo    "║                              Hello Suders !!                       
 echo    "║  Note: Now you have become Suder, but of course, you are not like Eng.Mina  ║"
 echo -e "╚═════════════════════════════════════════════════════════════════════════════╝\n"
 
+echo "+==================================+"
+echo "|     Super Menu                   |"
+echo "+==================================+"
+echo "| Option |     Description         |"
+echo "+--------+-------------------------+"
+echo "|   1    | Create Database         |"
+echo "|   2    | List Databases          |"
+echo "|   3    | Drop Database           |"
+echo "|   4    | Connect To Databases    |"
+echo "+--------+-------------------------+"
 
 
 
