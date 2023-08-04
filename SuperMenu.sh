ffb49e8c24e0012e8eb80bea81d56e65007fd47b
HuShaker <hushaker.dev@gmail.com>
2023-08-04 06:26:34 +0300

    Create error function

--- SuperMenu.sh
+++ SuperMenu.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SuperMenu
+source ./Extra_Functions/Error
 
-#configuration setting
+
+#============================================================ configuration setting ==================================================================
+
 clear
 #export PS1="SuperSUDER >> " 
 #export PS2=">> "
-
 echo    "╔═════════════════════════════════════════════════════════════════════════════╗"
 echo    "║                              Hello Suders !!                                ║"
 echo    "║  Note: Now you have become Suder, but of course, you are not like Eng.Mina  ║"
 echo -e "╚═════════════════════════════════════════════════════════════════════════════╝\n"
 
-#echo -e "\e[5m \033[31m   Error: Something went wrong. \033[31m  \e[0m"
-
-
-function super_menu {
+#============================================================ Super Menu ============================================================================
+function fun_super_menu {
+    
     echo "+==================================+"
     echo "|          Super Menu              |"
     echo "+==================================+"
@@ -31,19 +31,16 @@ function super_menu {
     read smenu
     case $smenu in
         1) echo "create_database" ;;
-        2) list_databases ;;
-        3) drop_database ;;
-        4) connect_to_databases ;;
+        2) fun_list_databases ;;
+        3) fun_drop_database ;;
+        4) fun_connect_to_databases ;;
         5) exit ;;
         *) 
-        # \033[31m ==> For Red Color &  \e[5m ==> for Animation
-        echo -e "\e[5m   \033[31m    Error: *** Wrong Choice. ***   \033[31m  \e[0m"
-
-
-        super_menu
+        fun_error "Error: *** Wrong Choice. *** "
+        fun_super_menu
     esac
 }
-super_menu
+fun_super_menu
 
 
 
