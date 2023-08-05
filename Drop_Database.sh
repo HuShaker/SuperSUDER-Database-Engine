c5960941027fd33a2f3ff10cb6ad93281d6faae8
HuShaker <hushaker.dev@gmail.com>
2023-08-06 02:07:55 +0300

    view list in drop db fun

--- Drop_Database.sh
+++ Drop_Database.sh
@@ -7,5 +7,12 @@ function fun_drop_database {
     echo "+==================================+"
     echo "|       Available Databases        |"
     echo "+==================================+"
+    counter=1
+    for db in "${arr[@]}"; do
+        db_name=$(basename "$db")
+        echo "|   $counter               $db_name "
+        echo "------------------------------------"
+        ((counter++))
+    done
 
 }
