aea1048cf2a60560df4065b1606a09485f623a04
HuShaker <hushaker.dev@gmail.com>
2023-08-06 02:07:08 +0300

    create drop db fun

---
+++ Drop_Database.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+function fun_drop_database {
+
+    clear
+    arr=(./Data/*)
+    echo "+==================================+"
+    echo "|       Available Databases        |"
+    echo "+==================================+"
+
+}
