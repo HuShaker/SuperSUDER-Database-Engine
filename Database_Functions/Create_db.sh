126e10f14051dc56f93a8309c7f8f3d2c9a9d1ae
shereenm1521 <shereinmakram@gmail.com>
2023-08-05 13:44:16 +0300

    validate fun

--- Database_Functions/Create_db.sh
+++ Database_Functions/Create_db.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/bash
 
+Validate_Name() {
+    # Regular expression to check for valid name (starts with a letter and contains only letters, numbers, or underscores)
+    pattern="^[a-zA-Z][a-zA-Z0-9_]*$"
 
+    if [[ $1 =~ $pattern ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 # Function to create the database
-create_database() {
+Create_database() {
     # Prompt the user to enter the database name
 read -p "Enter the database name: " db_name
 
      database_name="$1"
     if Validate_Name "$database_name"; then
-        if [ -d ../Databases$database_name ]; then
+        if [ -d "Databases/$database_name" ]; then
             echo "Error: Database already exists."
         else
-            mkdir ../Databases$database_name 
+            mkdir Databases/$database_name 
             echo "Database created successfully."
         fi
     else
@@ -21,4 +30,4 @@ read -p "Enter the database name: " db_name
 }
 
 # Call the create_database function with the user-provided database name
-create_database "$db_name"
+Create_database 
