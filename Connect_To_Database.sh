89c4d6c6ef61057d0f5e43cbb80f263edebb61ba
HuShaker <hushaker.dev@gmail.com>
2023-08-06 04:45:02 +0300

    add select option in connect db fun

--- Connect_To_Database.sh
+++ Connect_To_Database.sh
@@ -16,6 +16,26 @@ fun_connect_to_database () {
         ((counter++))
     done
     echo
+    read -p "Enter the number of the database: " connect_number
+
+    if [[ $connect_number =~ ^[0-9]+$ ]]; then
+        if [ "$connect_number" -ge 1 ] && [ "$connect_number" -le "${#arr[@]}" ]; then
+            connect_db="${arr[$((connect_number - 1))]}"
+            cd ./Data
+            $SHELL
+            echo ""
+        else
+            echo "Invalid database number."
+            sleep 2
+            fun_drop_database
+        fi
+    else
+        echo "Invalid input. Please enter a valid number."
+        sleep 2
+        fun_drop_database
+    fi
+
+
 
 
     cd ./Data
