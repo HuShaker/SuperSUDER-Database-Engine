42106aeac220820be1ce5f11ea98423c29387517
HuShaker <hushaker.dev@gmail.com>
2023-08-06 04:38:36 +0300

    update drop fun

--- Drop_Database.sh
+++ Drop_Database.sh
@@ -18,7 +18,18 @@ function fun_drop_database {
     read -p "Enter the number of the database: " delete_number
 
     if [[ $delete_number =~ ^[0-9]+$ ]]; then
-
+        if [ "$delete_number" -ge 1 ] && [ "$delete_number" -le "${#arr[@]}" ]; then
+            deleted_db="${arr[$((delete_number - 1))]}"
+            rm -r "$deleted_db"  # Delete the database directory
+            clear
+            echo "Database '$(basename "$deleted_db")' has been deleted."
+            sleep 2
+            fun_super_menu
+        else
+            echo "Invalid database number."
+            sleep 2
+            fun_drop_database
+        fi
     else
         echo "Invalid input. Please enter a valid number."
         sleep 2
