19505a4eee86872c18429a27e558b06769e60a12
HuShaker <hushaker.dev@gmail.com>
2023-08-06 04:34:59 +0300

    update drop fun

--- Connect_To_Database.sh
+++ Connect_To_Database.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/bash
 
-fun_connect_to_database() {
+fun_connect_to_database () {
+
+    clear
+    fun_header_note
+    arr=(./Data/*)
+    echo "+==================================+"
+    echo "|       Available Databases        |"
+    echo "+==================================+"
+    counter=1
+    for db in "${arr[@]}"; do
+        db_name=$(basename "$db")
+        echo "|   $counter               $db_name "
+        echo "------------------------------------"
+        ((counter++))
+    done
+    echo
+
+
     cd ./Data
+    $SHELL
+    echo ""
+
+
 }
