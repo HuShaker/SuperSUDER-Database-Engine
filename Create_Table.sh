35a3fe4b877ec8de64252d029063d6e9d6f56cc1
HuShaker <hushaker.dev@gmail.com>
2023-08-07 01:41:00 +0300

    Update: create table fun

--- Create_Table.sh
+++ Create_Table.sh
@@ -10,6 +10,14 @@ fun_create_table() {
     read tableName
     # check the table name is valid or not
     if fun_validate_name "$tableName"; then
+        # Valid Name
+        #Check if the table exist or not
+        if [ -e "./${tableName}_data" ] && [ -e "./${tableName}_metadata" ]; then
+            echo "The Table already exists."
+            sleep 2
+            clear
+            fun_create_table
+        fi
         
 
     else
