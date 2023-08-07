3f3ad8dec8d60485e9745746329580a9e14c7cdf
HuShaker <hushaker.dev@gmail.com>
2023-08-07 04:12:30 +0300

    Add: feature in drop table

--- Drop_Tables.sh
+++ Drop_Tables.sh
@@ -14,5 +14,25 @@ fun_drop_table() {
         ((counter++))
     done
     echo
+    read -p "Enter the number of the Table: " delete_number
 
+    if [[ $delete_number =~ ^[0-9]+$ ]]; then
+        if [ "$delete_number" -ge 1 ] && [ "$delete_number" -le "${#mytables[@]}" ]; then
+            deleted_table="${mytables[$((delete_number - 1))]}"
+            rm -r "${deleted_table}_metadata" "${deleted_table}_data"  # Delete the table directory
+            clear
+            echo "Table ${deleted_table} has been deleted."
+            sleep 3
+            clear
+            fun_table_menu
+        else
+            echo "Invalid table number."
+            sleep 3
+            fun_drop_table
+        fi
+    else
+        echo "Invalid input. Please enter a valid number."
+        sleep 3
+        fun_drop_table
+    fi
 }
