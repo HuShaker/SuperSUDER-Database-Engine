b114a03c552d1177aa1cd2098b049aab5a058fee
shereenm1521 <shereinmakram@gmail.com>
2023-08-11 20:41:09 +0300

    test again updaate

--- delete_from_table.sh
+++ delete_from_table.sh
@@ -18,4 +18,55 @@ delete_one_row(){
         #clear
     fi 
 }
+elete_multi_row(){
+
+    fun_choose_table
+    read -p "Enter the first ID Number: " first_ID_Number
+    read -p "Enter the last ID Number: " last_ID_Number
+    fRow=$(awk -F':' '$1=="'$first_ID_Number'"' "./${current_table}_data")
+    lRow=$(awk -F':' '$1=="'$last_ID_Number'"' "./${current_table}_data")
+
+    if [ "$fRow" == "" ] || [ "$lRow" == "" ]; then
+        
+        echo "ID number not exist"
+        sleep 3
+        clear
+    else
+        clear
+        sed -i "/$first_ID_Number/,/$last_ID_Number/d" "./${current_table}_data"
+        echo "the rows have been deleted..."
+        sleep 3
+        #clear
+    fi 
+}
+
+fun_delete_from_table() {
+
+    select option in "One Row" "Multi Rows" "All Rows"; do
+        case $option in
+            "One Row")
+                delete_one_row
+                sleep 3
+                fun_table_menu
+                ;;
+            "Multi Rows")
+                delete_multi_row
+                fun_table_menu
+                ;;
+            "All Rows")
+                echo -n "" > "./${current_table}_data"
+                 echo "the rows have been deleted..."
+                sleep 3
+                clear
+                fun_table_menu
+                ;;
+            *)
+                echo "Invalid selection. Please choose again."
+                sleep 3
+                clear
+                fun_table_menu
+                ;;
+        esac
+    done
+}
 
--- select_from_table.sh
+++ select_from_table.sh
@@ -1,53 +1,124 @@
 #!/usr/bin/bash
-select_all_table () {
-    
-
-    ls -F |grep -v 'metadata' 
-    echo -e "Enter the name of the table you want to select from : \c"
-    read tname
-  
-    if [[ -f ${tname} ]]; then
-        cat ${tname}
-    else
-        echo "Error displaying table $tname"
-    fi
-}
 
 
-select_one_row () {
-    ls -F |grep -v 'metadata' 
-    echo -e "Enter the name of the table you want to select from : \c"
-    read tname
-  
-    if [[ -f ${tname} ]]; then
-    echo 'enter the id number :'
-    read -r number_
-    if  [[  ! $number =~ ^[0-9]+$ ]] ;
-    then
-    echo "error"
-    else
-    sed -n "$number p" $tname
-    echo "the row $number has been selected:"
-    fi
-}
+fun_choose_table() {
 
-fun_select_from_table  () {
-    echo "enter your option :"
-       select option in select_all_table select_one_row 
-       do 
-
-       case $option in 
+    #clear
+    mytables=($(ls -F | grep -v '_metadata' | sed 's/_data$//'))
+    counter=1
+    echo "+==================================+"
+    echo "|       Available Tables           |"
+    echo "+==================================+"
+    for table in "${mytables[@]}"; do
+        echo "|   $counter               $table "
+        echo "------------------------------------"
+        ((counter++))
+    done
+    echo
+    read -p "Enter the number of the Table: " table_number
+    if [[ $table_number =~ ^[0-9]+$ ]]; then
+        if [ "$table_number" -ge 1 ] && [ "$table_number" -le "${#mytables[@]}" ]; then
+            #Get Table Name
+            current_table="${mytables[$((table_number - 1))]}"
             
-            1 ) 
-             select_all_table ;; 
+        else
+            echo "Invalid table number."
+            sleep 3
+            fun_choose_table
+        fi
+    else
+        echo "Invalid input. Please enter a valid number."
+        sleep 3
+        fun_choose_table
+    fi
 
-             2 ) 
-             select_one_row   ;; 
-
-             * ) 
-             echo " invalid option :" ;; 
-
-         esac
+}
 
 
+fun_select_row() {
+
+    read -p "Enter the ID Number: " idNumber
+    row=$(awk -F':' '$1=="'$idNumber'"' "./${current_table}_data") 
+    if [ "$row" == "" ]; then
+        
+        echo "ID number not exist"
+        sleep 3
+        clear
+    else
+        clear
+        echo "$row"
+        echo
+        sleep 3
+    fi 
+
+}
+
+fun_select_by_id() {
+
+    read -p "Enter the ID Number: " idNumber
+    # Read Metadata
+    column_names=($(head -n 1 "./${current_table}_metadata" | tr ':' ' '))
+
+    # Select search column from table columns
+    counter=1
+    echo "╔══════════════════════════════════╗"
+    echo "║           Column Names           ║"
+    echo "╠════════╦═════════════════════════╣"
+    echo "║ Option ║     Description         ║"
+    echo "╠════════╩═════════════════════════╣"
+    for column in "${column_names[@]}"; do
+        echo "║   $counter               $column "
+        if [ "$column" == "${column_names[-1]}" ]; then
+            echo "╚══════════════════════════════════╝"
+        else
+            echo "╠══════════════════════════════════╣"
+        fi
+        ((counter++))
+    done
+    echo
+    read -p "Enter the column number: " key_column_number
+
+    if fun_validate_number "$key_column_number" && [ "$key_column_number" -ge 1 ] && [ "$key_column_number" -le "${#column_names[@]}" ]; then
+            key_col_index=$((key_column_number - 1))
+            key_column_name="${column_names[$key_col_index]}"
+            row=$(awk -F':' '$1=="'$idNumber'"' "./${current_table}_data") 
+            currentRow=($(echo "$row" | tr ':' ' '))
+            clear
+            echo "The ${key_column_name} is: ${currentRow[$key_col_index]}"
+    else
+        echo "Invalid column number..."
+        sleep 3
+        clear
+        fun_Select_from_table
+    fi
+
+}
+
+fun_Select_from_table() {
+
+    select option in "All" "Row" "ByID"; do
+        case $option in
+            "All")
+                fun_choose_table
+                cat "${current_table}_data"
+                sleep 3
+                fun_table_menu
+                ;;
+            "Row")
+                fun_choose_table
+                fun_select_row
+                fun_table_menu
+                ;;
+            "ByID")
+                fun_choose_table
+                fun_select_by_id
+                fun_table_menu
+                ;;
+            *)
+                echo "Invalid selection. Please choose again."
+                sleep 2
+                fun_table_menu
+                ;;
+        esac
+    done
 }
