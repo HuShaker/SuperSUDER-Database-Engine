2f638b5b0699d506cb6b8c0ffa5be74005c6ff4f
HuShaker <hushaker.dev@gmail.com>
2023-08-11 18:56:08 +0300

    update: multi row feature

--- Delete_Table.sh
+++ Delete_Table.sh
@@ -27,7 +27,18 @@ delete_multi_row(){
     fRow=$(awk -F':' '$1=="'$first_ID_Number'"' "./${current_table}_data")
     lRow=$(awk -F':' '$1=="'$last_ID_Number'"' "./${current_table}_data")
 
-
+    if [ "$fRow" == "" ] || [ "$lRow" == "" ]; then
+        
+        echo "ID number not exist"
+        sleep 3
+        clear
+    else
+        clear
+        sed -i "/$first_ID_Number/,/$last_ID_Number/d" "./${current_table}_data"
+        echo "the rows have been deleted..."
+        sleep 3
+        #clear
+    fi 
 }
 
 
